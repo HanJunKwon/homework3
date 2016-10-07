9d4f3a07a6a2c3c0e5bd2fc678446a5ecb65ef0c
권한준 <skclaqks11@naver.com>
2016-10-07 17:37:45 +0900

    B04.java 추가

---
+++ B04.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class B04{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int salary=0;
+		System.out.print("연봉을 원단위로 입력하세요");
+		salary = sc.nextInt();
+		if(salary<10000000)
+			System.out.printf("연봉 금액에 대한 소득세는 %d입니다",salary/9.5);
+		else if(salary<40000000)
+			System.out.printf("연봉 금액에 대한 소득세는 %d입니다",salary/19);
+		else if(salary<80000000)
+			System.out.printf("연봉 금액에 대한 소득세는 %d입니다",salary/28);
+		else
+			System.out.printf("연봉 금액에 대한 소득세는 %d입니다",salary/37);
+
+	}
+}
